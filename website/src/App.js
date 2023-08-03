e161f7bae2542eb55afa02484e3e7937b49a2cae
EConradie <221147@virtualwindow.co.za>
2023-08-03 11:07:13 +0200

    setup

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Landing from './pages/landing';
+import Compare from './pages/compare';
+import Timeline from './pages/timeline';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BasicNav from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicNav />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/time' element={<Timeline />} />
+        <Route path='/compare' element={<Compare />} />
+      </Routes>
     </div>
   );
 }
